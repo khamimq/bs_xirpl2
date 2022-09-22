70d3f040f91a3c63644243c926f942d46bdad7e9
khamimq <khamim.q@gmail.com>
2022-09-22 13:41:13 +0700

    app web profile v 1.0

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to CodeIgniter 4!</title>
+    <meta name="description" content="The small framework with powerful features">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+    <link rel="stylesheet" href="<?=base_url('bootstrap/css/bootstrap.min.css')?>">
+</head>
+<body>
+    <nav class="navbar navbar-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar scroll</a>
+        </div>
+    </nav>
+    <div class="container-fluid">
+        <div class="col-md-2 col-lg-2">
+            <div class="card">
+                <div class="card-header">Main Menu</div>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <a href="#" class="nav-link">Paket</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#" class="nav-link">Member</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-10 col-lg-10">
+
+        </div>
+    </div>
+</body>
+</html>
+<script src="<?=base_url('bootstrap/js/bootstrap.min.js')?>"></script>
